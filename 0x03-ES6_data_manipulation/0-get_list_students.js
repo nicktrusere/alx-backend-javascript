b72d702686335ba0c1b57c970798746d3d544e21
Nicktrusere <Nicktrusere@gmail.com>
2023-10-02 10:04:29 -0700

    Basic list of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export default function getListStudents() {
+	return [
+		{ firstName: 'Guillaume', id: 1, loaction:'San Francisco' },
+		{ firstName: 'James', id: 2, loaction:'Columbia' },
+		{ firstName: 'Serena', id: 5, loaction:'San Francisco' },
+	};
+}
---
+++ 0x03-ES6_data_manipulation/README.md
@@ -0,0 +1,1 @@
+This is a 0x03. ES6 data manipulation readme.md file.
