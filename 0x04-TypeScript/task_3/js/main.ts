7b3fa346110e0bf0c49def8d57eef059ca7398ee
Nicktrusere <Nicktrusere@gmail.com>
2023-10-04 11:44:40 -0700

    updating Task_3

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -1,1 +1,19 @@
-'use strict';
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path = "./crud.d.ts" />
+
+import * as CRUD from './crud';
+import { RowElement, RowID } from './interface';
+
+const row: RowElement = {
+  firstName: 'Guillaume',
+  lastName: 'Salva',
+};
+
+const updatedRow: RowElement = {
+  ...row,
+  age: 23,
+};
+
+const rowID: RowID = CRUD.insertRow(row);
+CRUD.updateRow(rowID, updatedRow);
+CRUD.deleteRow(rowID);
