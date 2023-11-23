0b058875cba4b0a258a7045c2f7cf962ea1195d7
Nicktrusere <Nicktrusere@gmail.com>
2023-11-23 15:55:29 +0000

    Revised Intergration test

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,14 +1,13 @@
 const express = require('express');
 
 const app = express();
-const port = 7865;
 
-app.get('/', (request, response) => {
-  response.send('Welcome to the payment system');
+app.get('/', (req, res) => {
+  res.send('Welcome to the payment system');
 });
 
-app.listen(port, () => {
-    console.log("API available on localhost port 7865");
+app.listen(7865, '0.0.0.0', () => { 
+  console.log('API available on localhost port 7865');
 });
 
 module.exports = app;
--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,28 +1,29 @@
-const request = require("request");
-const {describe, it} = require("mocha");
-const expect = require("chai").expect;
+const { expect } = require('chai');
+const request = require('request');
 
-describe("Index page", function() {
-    const options = {
-	url: "http://localhost:7865/",
-	method: "GET"
-    }
-    it("check correct status code", function(done) {
-	request(options, function(err, res, body) {
-	    expect(res.statusCode).to.equal(200);
-	    done();
-	});
+describe('api', () => {
+  const options = {
+    url: 'http://localhost:7865',
+    method: 'GET'
+  }
+  it('test correct status code', (done) => {
+    request(options, (err, res, body) => {
+      expect(res.statusCode).to.equal(200);
+      done();
     });
-    it("check correct content", function(done) {
-	request(options, function(err, res, body) {
-	    expect(body).to.contain("Welcome to the payment system");
-	    done();
-	});
+  });
+
+  it('test correct response body', (done) => {
+    request(options, (err, res, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
     });
-    it("check correct content length", function(done) {
-	request(options, function(err, res, body) {
-	    expect(res.headers['content-length']).to.equal('29');
-	    done();
-	});
+  });
+
+  it('test correct content type', (done) => {
+    request(options, (err, res, body) => {
+      expect(res.headers['content-type']).to.equal('text/html; charset=utf-8');
+      done();
     });
+  });
 });
